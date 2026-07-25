d5b3378fa955afa5229c4df4ea796f0eac36ba11
smartrat4life <ratsmart92@gmail.com>
2026-07-25 13:57:32 +0100

    Update ApplicationSuccess.tsx

--- src/pages/ApplicationSuccess.tsx
+++ src/pages/ApplicationSuccess.tsx
@@ -204,7 +204,7 @@ const ApplicationSuccess: React.FC = () => {
                     </li>
                     <li className="flex items-start gap-2">
                       <Phone className="w-5 h-5 text-teal-600 flex-shrink-0 mt-0.5" />
-                      <span>If approved, we'll schedule a call to answer questions and arrange a meet-and-greet.</span>
+                      <span></span>
                     </li>
                   </ul>
                 </div>
